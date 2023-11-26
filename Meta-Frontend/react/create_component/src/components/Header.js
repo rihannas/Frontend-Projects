da12be26fbc3c99493b88c06cd2239c54b3ba2a4
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-11-26 23:02:46 +0300

    inline css

--- Meta-Frontend/react/create_component/src/components/Header.js
+++ Meta-Frontend/react/create_component/src/components/Header.js
@@ -1,6 +1,11 @@
 
 function Header(props){
-    return <h1>Hello from {props.name} my fav color is {props.color}</h1>
+    const style = {
+        color : "purple",
+        fontSize: "80px",
+
+    }
+    return <h1 style={style}>Hello from {props.name} my fav color is {props.color}</h1>
 }
 
 export default Header;
