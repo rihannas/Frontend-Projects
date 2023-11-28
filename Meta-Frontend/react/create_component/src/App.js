04dda96d9a59f0cf77c4420ab3d38fc96f71ec49
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-11-28 03:53:52 +0300

    Add RegisterForm component

--- Meta-Frontend/react/create_component/src/App.js
+++ Meta-Frontend/react/create_component/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 import Card from './components/Card';
-
+import RegisterForm from './components/Form';
 function App() {
   return (
     <div>
@@ -16,7 +16,7 @@ function App() {
         <Card h2="2nd card's h2" h3="2nd card's h3"/>
         <Card h2="3rd card's h2" h3="3rd card's h3"/>
       </div>
-      
+      <RegisterForm />
     </div>
     
     );
