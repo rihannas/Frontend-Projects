9174aa94cb9efcaa0d4ac2142afcd72a0e9a0195
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-12-01 01:43:05 +0300

    prop drilling

--- Meta-Frontend/react/create_component/src/App.js
+++ Meta-Frontend/react/create_component/src/App.js
@@ -1,11 +1,20 @@
 // import logo from './logo.svg';
+import React from "react";
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 import Card from './components/Card';
 import RegisterForm from './components/Form';
+import FavColor from './components/FavColor';
+import Fruits from "./components/Fruits";
+import FruitsCounter from "./components/FruitsCounter";
 function App() {
+  const [fruits] = React.useState([
+        {fruitName: 'apple', id: 1},
+        {fruitName: 'apple', id: 2},
+        {fruitName: 'plum', id: 3},
+    ]);
   return (
     <div>
       <Header name='jk' color='purple'/>
@@ -17,7 +26,16 @@ function App() {
         <Card h2="3rd card's h2" h3="3rd card's h3"/>
       </div>
       <RegisterForm />
+      <FavColor/>
+
+      <div>
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits} />
+      <FruitsCounter fruits={fruits}/>
     </div>
+    </div>
+
+    
     
     );
 }
